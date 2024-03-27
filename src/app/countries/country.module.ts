fd160ad3141987fc713cdabcd7c91ad797f800ee
KennerEspinal <kennerespinalg@gmail.com>
2024-03-27 14:07:11 -0500

    Se termina la Sección 19

--- src/app/countries/country.module.ts
+++ src/app/countries/country.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
 import { CountriesRoutingModule } from './countries-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
     imports: [
         CommonModule,
-        CountriesRoutingModule
+        CountriesRoutingModule,
+        ReactiveFormsModule
     ],
     exports: [],
     declarations: [
--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountriesService } from '../../services/countries.service';
+import { Region, SmallCountry } from '../../interfaces/country.interface';
+import { filter, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
   styles: ``
 })
-export class SelectorPageComponent {
+export class SelectorPageComponent implements OnInit {
+
+  countriesByRegions: SmallCountry[] = [];
+  borders: SmallCountry[] = [];
+
+  myForm: FormGroup = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    border: ['', Validators.required]
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private countriesService: CountriesService
+  ) { }
+
+  ngOnInit(): void {
+    this.onRegionChange();
+    this.onCountryChange();
+  }
+
+  get regions(): Region[] {
+    return this.countriesService.regions;
+  }
+
+  onRegionChange(): void {
+    this.myForm.get('region')!.valueChanges
+      .pipe(
+        tap(() => this.myForm.get('country')!.setValue('')),
+        tap(() => this.borders = []),
+        switchMap((region) => this.countriesService.getCountriesByRegion(region))
+      )
+      .subscribe(countries => {
+        this.countriesByRegions = countries;
+      });
+  }
+
+  onCountryChange(): void {
+    this.myForm.get('country')!.valueChanges
+      .pipe(
+        tap(() => this.myForm.get('border')!.setValue('')),
+        filter((value: string) => value.length > 0),
+        switchMap((alphaCode) => this.countriesService.getCountryByCca3(alphaCode)),
+        switchMap((country) => this.countriesService.getCountryBordersByCodes(country.borders))
+      )
+      .subscribe(countries => {
+        this.borders = countries;
+      });
+  }
 
 }
